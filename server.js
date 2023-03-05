7151faa5b3211c0110fb97409dffa2252a727cf2
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-05 21:28:58 +0300

    create server.js and app.js file. create an express app and import it inside server.js and put everything related to server configuration inside server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`App Listning to port: ${port}`);
+});
