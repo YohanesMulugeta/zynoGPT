5acc779b556ff70995d68284be1791849ae44f1c
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-06 13:46:41 +0300

    install jsonwebtoken. and Implement keeping loged in when a user signed up by sending jwt token in the response and coockie.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,53 @@
+const jwt = require("jsonwebtoken");
 const User = require("../model/user");
 
-exports.signUp = async function (req, res, next) {
-  const { name, email, password, passwordConfirm, photo, plan } = req.body;
+function signAndSend(user, statusCode, res) {
+  const token = jwt.sign(
+    { id: user._id, iat: Date.now() / 1000 - 30 },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES,
+    }
+  );
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-    passwordConfirm,
-    photo,
-    plan,
-  });
+  const cookieOpt = {
+    expires: Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  res.cookie("jwt", cookieOpt);
 
   user.password = undefined;
 
-  res.status(201).json({
+  res.status(statusCode).json({
     status: "success",
-    data: { user },
+    token,
+    user,
   });
+}
+
+exports.signUp = async function (req, res, next) {
+  try {
+    const { name, email, password, passwordConfirm, photo, plan } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+      photo,
+      plan,
+    });
+
+    user.password = undefined;
+
+    signAndSend(user, 201, res);
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+      err,
+    });
+  }
 };
