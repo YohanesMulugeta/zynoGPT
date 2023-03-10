4bc535295c8d7652538ca448ca1e7e0f8f53237f
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-10 12:11:18 +0300

    create a features-description.json file inside the dev-data dir that will be used to configure features page and feature despilaying.

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,6 +2,79 @@ const catchAsync = require("../util/catchAsync");
 const AppError = require("../util/AppError");
 const { render } = require("pug");
 
+const features = {
+  leaveletterwriting: {
+    head1: "Write your Name here.",
+    head2:
+      "Write the person's name or role in which you are going to send the letter here.",
+    head3: "Write the number of days you want leave.",
+    head4: "Write a small description of the reason here.",
+  },
+  coverletterwriting: {
+    head1: "Write your Name here.",
+    head2: "Write your Desired Job Title here.",
+    head3: "Write your Skill set here.",
+    head4: "Write your a small description about yourself here.",
+  },
+  essaywriting: {
+    head1: "Write your Essay Title here.",
+    head2: "Write a small description here.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  assignmentwriting: {
+    head1: "Write your Assignment Title here.",
+    head2: "Write a small description here.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  brandnamegenerator: {
+    head1: "Write your desired words to be included.",
+    head2:
+      "Write a small description you have in your mind about your business.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  socialmediacaptions: {
+    head1: "Write your post's Title here.",
+    head2: "Write a small description here.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  textexpander: {
+    head1: "Paste your original content here.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  textparaphrase: {
+    head1: "Paste your original content here.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  startupideasgenerator: {
+    head1: "Write your idea in a single line.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  hashtagsgenerator: {
+    head1: "Write your idea in a single line.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  youtubevideodescriptiongenerator: {
+    head1: "Write your video title here.",
+    head2: "Write some keywords related to video.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+  youtubevideoscriptgenerator: {
+    head1: "Write your desired video title here.",
+    head2: "Write some keywords related to video.",
+    featureDescription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae velit nam consectetur, in ullam natus pariatur aliquid. Aut, magnam!",
+  },
+};
+
 function renderError(
   errMessage,
   statusCode,
@@ -68,7 +141,15 @@ exports.feature = function (req, res, next) {
       res
     );
 
-  res.render("feature", { title: feature });
+  const title = feature
+    .split("-")
+    .map((str) => str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase())
+    .join(" ");
+
+  res.render("feature", {
+    title,
+    ...features[feature.split("-").join("")],
+  });
 };
 
 exports.faqs = function (req, res, next) {
