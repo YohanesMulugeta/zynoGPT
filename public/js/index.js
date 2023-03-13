ae823f8be47a4cfec5696284f84e9e917de663bb
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-13 15:40:48 +0300

    programatically set ids to the feature form depending the feature title. and also programatically select feature form depending on the document title.

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,11 @@ const login = document.querySelector(".login-form");
 const signup = document.querySelector(".signup-form");
 const logout = document.querySelector(".btn-logout");
 
+// FEATURES
+const feature = document.getElementById(
+  document.title.toLowerCase()?.split("|")[1]?.trim()?.split(" ")?.join("-")
+);
+
 login?.addEventListener("submit", handleLogin);
 logout?.addEventListener("click", handleLogout);
 signup?.addEventListener("submit", handleSignup);
-
-console.log(signup);
